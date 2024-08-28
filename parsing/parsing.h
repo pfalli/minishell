03c1b9e81ceff820952477cc8150a52e372cded9
atamas <tamas.andor@yahoo.com>
2024-08-28 15:10:29 +0200

    Include stdio library before readline for Clion debugger and norminette error.c

--- parsing/parsing.h
+++ parsing/parsing.h
@@ -20,11 +20,11 @@
 # include <dirent.h>
 # include <errno.h>
 # include <fcntl.h>
+# include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
 # include <stdbool.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/stat.h>
